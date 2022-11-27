deaac6d28044cd08692f8fec82a95fd4b9a42fa1
Nguyễn Lâm Nhật Minh <99701824+nhatminh3004@users.noreply.github.com>
2022-11-28 01:10:05 +0700

    Update APIRoutes.js

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,4 +1,4 @@
-export const host = "http://chat-realtime-socket-server.herokuapp.com";
+export const host = "https://chat-realtime-socket-server.herokuapp.com";
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
